1bcaebc8f1a1a4ba053aa77f2062d0a086740b43
manas.santra@awscloude.com+1-at-097579254369 <47280250+manassantra@users.noreply.github.com>
2023-08-09 03:22:37 +0530

    Development Process v2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidenavComponent } from './common/sidenav/sidenav.component';
+import { NavbarComponent } from './common/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SidenavComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+  brandLogo_url = 'https://www.logolynx.com/images/logolynx/57/572426d3d28ca8447b8e02a0e0d48f2e.jpeg';
+}
